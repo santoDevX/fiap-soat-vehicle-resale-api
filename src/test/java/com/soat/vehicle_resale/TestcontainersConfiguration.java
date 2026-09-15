a798630be32b4fb022ba4be4d2e260f2eab4d99a
Anderson Santo <andersonsanto08@gmail.com>
2026-09-15 19:48:36 -0300

    chore: clean up comments in TestcontainersConfiguration for clarity

--- src/test/java/com/soat/vehicle_resale/TestcontainersConfiguration.java
+++ src/test/java/com/soat/vehicle_resale/TestcontainersConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
-// publica para poder ser importada pelos testes de integracao de outros pacotes
-// (ex: infrastructure.adapters.outbound.database.repositories), reaproveitando o mesmo container.
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
 
